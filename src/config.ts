906cec32c33efc9c3edcaa5435fc98a3038c6316
sanjaysr77 <sanjaysrbangalore@gmail.com>
2025-03-05 04:43:59 +0530

    Changes in config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,22 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const JWT_PASSWORD = process.env.JWT_PASSWORD as String;
+
+//  console.log(JWT_PASSWORD)
+
+
+// if (!JWT_PASSWORD) {
+//     throw new Error("JWT_PASSWORD is not defined. Check your .env file.");
+// }
+
 import dotenv from "dotenv";
 dotenv.config();
-export const JWT_PASSWORD = process.env.JWT_PASSWORD || "your-secret-key"
 
-// console.log(JWT_PASSWORD)
-
-
-if (!JWT_PASSWORD) {
+if (!process.env.JWT_PASSWORD) {
     throw new Error("JWT_PASSWORD is not defined. Check your .env file.");
 }
+
+export const JWT_PASSWORD  = process.env.JWT_PASSWORD;
+
+console.log(JWT_PASSWORD);
